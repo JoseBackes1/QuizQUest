c55908c1d199ae8d7c5022db321e3fd2b650f04c
José Carlos Backes da Luz <srbackes@hotmail.com>
2024-06-19 16:09:36 -0300

    alteração parametro banco

--- QuizQuest/src/main/java/com/example/QuizQuest/DatabaseConfig.java
+++ QuizQuest/src/main/java/com/example/QuizQuest/DatabaseConfig.java
@@ -3,5 +3,5 @@ package com.example.QuizQuest;
 public class DatabaseConfig {
     public static final String URL = "jdbc:postgresql://localhost:5432/quizDB";
     public static final String USER = "postgres";
-    public static final String PASSWORD = "253359";
+    public static final String PASSWORD = "senhaQualquer";
 }
